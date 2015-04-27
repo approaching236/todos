1a1e6ba2f8ab41cce4415033b00bdc08cbb4613e
Vincent Grato <vgrato@gmail.com>
2015-04-27 10:39:42 -0700

    Fail to make a new route / query

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,16 +1,16 @@
---name: create-user!
--- creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+--name: create-todo!
+-- creates a new todo record
+INSERT INTO todos
+(project, description, issue, blocker, callbacks)
+VALUES (:project, :description, :issue, :blocker, :callbacks)
 
---name: update-user!
--- update an existing user record
-UPDATE users
+--name: update-todo!
+-- update an existing todo record
+UPDATE todos
 SET first_name = :first_name, last_name = :last_name, email = :email
 WHERE id = :id
 
--- name: get-user
+-- name: get-todo
 -- retrieve a used given the id.
-SELECT * FROM users
+SELECT * FROM todos
 WHERE id = :id
--- src/todo/routes/todos.clj
+++ src/todo/routes/todos.clj
@@ -1,6 +1,8 @@
 (ns todo.routes.todos
   (:require [todo.layout :as layout]
-            [compojure.core :refer [routes GET]]))
+            [compojure.core :refer [routes GET]]
+            [ring.util.response :refer [redirect]]
+            [guestbook.db.core :as db]))
 
 (defn index []
   (layout/render "todos/index.html"))
@@ -9,7 +11,17 @@
   (layout/render "todos/show.html"
                  {:id id}))
 
+(defn new-todo [id]
+  (do
+    (db/create-todo! "todo"
+                  "be a badass"
+                  "I'm almost too badass to be more badass"
+                  "nothing is stopping me"
+                  "tell everyone")
+    (redirect "/todos")))
+
 (defn todos-routes []
   (routes
     (GET "/" [] (index))
+    (GET "/new" [] (redirect "/todos"))
     (GET "/:id" [id] (show id))))
